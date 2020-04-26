06260bad8c334694b2d92fde7e4fa5590470eb28
Laurens Van Houtven (lvh) <_@lvh.io>
2020-04-25 21:27:27 -0500

    An implementation of regexes via instaparse

---
+++ src/latacora/wernicke/insta_regex.clj
@@ -0,0 +1,68 @@
+(ns latacora.wernicke.insta-regex
+  (:require
+   [instaparse.core :as i]
+   [instaparse.combinators :as ic]
+   [com.gfredericks.test.chuck.regexes :as cre]
+   [taoensso.timbre :refer [spy]]
+   [com.rpl.specter :as sr]
+   [eidolon.core :as ec]
+   [clojure.core.match :as m]
+   [clojure.string :as str]))
+
+(def pattern? (partial instance? java.util.regex.Pattern))
+
+(def ^:private RE-PARSE-ELEMS-POST
+  "A navigator for all the parsed elements in a test.chuck regex parse tree in post-order.
+
+  Stops at each regex part and recursively navigates down into every element."
+  ;; TODO: see if the other uses of RE-PARSE-ELEMS still need this and still need it in a specific order
+  (sr/recursive-path [] p [(sr/continue-then-stay :elements sr/ALL p)]))
+
+(defn ^:private convert
+  [regex]
+  (let [regex (str regex) ;; might be a pattern
+        parsed (cre/parse regex)]
+    (sr/replace-in
+     [RE-PARSE-ELEMS-POST]
+     (fn [{:keys [type elements] :as re-part}]
+       (case type
+         :character [(-> re-part :character str ic/string) nil]
+
+         :concatenation [(apply ic/cat elements) nil]
+
+         :alternation [(apply ic/alt elements) nil]
+
+         :repetition
+         (let [[min max] (:bounds re-part)
+               max (or max ##Inf)]
+           [(->> elements (apply ic/cat) (ic/rep min max)) nil])
+
+         :class
+         ;; classes are quite complicated and can't themselves be (named)
+         ;; groups, which is the entire point of this parser; so we delegate
+         ;; them to the actual regex implementation
+         [(->> re-part i/span (apply subs regex) re-pattern ic/regexp)
+          nil]
+
+         (:class-union :class-intersection :class-base :range)
+         [nil nil] ;; handled by :class via regex
+
+         :group
+         (if-some [group-name
+                   (m/match
+                    [(:flag re-part)]
+                    [[:GroupFlags [:NamedCapturingGroup [:GroupName n]]]] (keyword n)
+                    :else nil)]
+           ;; telling instaparse about named groups is the only reason we use replace-in
+           [(ic/nt group-name) {group-name (apply ic/cat elements)}]
+           [(apply ic/cat elements) nil])))
+     parsed
+     :merge-fn merge)))
+
+(defn parser
+  "Given a regex (string or pattern), parse it into an Instaparse parser."
+  [regex]
+  (let [[root rules] (convert regex)]
+    (-> rules
+        (assoc :S root)
+        (i/parser :start :S))))
---
+++ test/latacora/wernicke/insta_regex_test.clj
@@ -0,0 +1,30 @@
+(ns latacora.wernicke.insta-regex-test
+  (:require
+   [instaparse.core :as i]
+   [latacora.wernicke.insta-regex :as ir]
+   [clojure.test :as t]
+   [latacora.wernicke.core :as wc]
+   [clojure.test.check.generators :as gen]
+   [clojure.test.check.clojure-test :as tct]
+   [com.gfredericks.test.chuck.clojure-test :as tct']
+   [com.gfredericks.test.chuck.generators :as gen']
+   [com.gfredericks.test.chuck.regexes :as cre]
+   [taoensso.nippy :as nippy]))
+
+(def test-patterns
+  "Some test patterns we want to turn into instaparse parsers.
+
+  Sorted by increasing (proxy for) complexity so that generator shrinking does
+  something useful. The proxy for complexity here is the (EDN) serialization of
+  the test.chuck parse tree."
+  (->> @#'wc/default-rules
+       (map ::wc/pattern)
+       (into [#"a" #"a*(b|c){1,10}x+"])
+       (sort-by (fn [r] (-> r str cre/parse pr-str count)))))
+
+(tct/defspec instaparse-equivalents-match-regexes
+  (tct'/for-all
+   [regex (gen/elements test-patterns)
+    s (gen'/string-from-regex regex)]
+   ;; TODO: write test ensuring that the resulting parser has the correct named groups
+   (t/is (some? (i/parse (ir/parser regex) s)))))
